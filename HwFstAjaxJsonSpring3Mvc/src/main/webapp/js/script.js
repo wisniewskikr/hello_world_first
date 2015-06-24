afbf028b8db374d4c585bf4f06003272071168c0
Krzysztof Wisniewski <wisniewskikr@gmail.com>
2015-06-24 10:41:29 +0200

    Improvement in project HwFstAjaxJsonSpring3Mvc

--- HwFstAjaxJsonSpring3Mvc/src/main/webapp/js/script.js
+++ HwFstAjaxJsonSpring3Mvc/src/main/webapp/js/script.js
@@ -5,25 +5,15 @@
 	
 	$("#inputForm").submit(function() {
 		
-		var dataObj = {
+		var json = {
 				name: $('#name').val()					
 		};
-		var dataJson = JSON.stringify(dataObj, null, 2);
-		
-//		var name = $('#name').val();
-//		var json = { "name" : name};
-
 				    
 	   $.ajax({
 	        type: "POST",
-//	        data: JSON.stringify(json),
 	        contentType : 'application/json',
 	        url: "app/ajax",
-	        data: dataJson,
-//	        beforeSend: function(xhr) {
-//	    		xhr.setRequestHeader("Accept", "application/json");
-//	    		xhr.setRequestHeader("Content-Type", "application/json");
-//	    	},
+	        data: JSON.stringify(json),
 	        success: function(response){
 	        	
 	        	if(response.status == "SUCCESS") {
@@ -39,19 +29,6 @@
 		    	
 	        }
 	   });
-		
-//		$.postJSON("app/ajax", json, function(data) {
-//			if(response.status == "SUCCESS") {
-//        		$("#inputContent").hide();
-//		    	$("#outputContent").show();
-//		    	$("#result").text(response.name);
-//		    	$("#errors").text("");
-//        	}
-//        	
-//        	if(response.status == "FAIL") {
-//        		$("#errors").text(response.message);
-//        	}
-//	    });
 	   return false;
 	   
 	});
